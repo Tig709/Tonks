17a23ae40233a24b3bd7862f91fb7a7f2a9934f7
Joey <joey.loos@hva.nl>
2022-03-22 11:01:31 +0100

    Base upgrade template

---
+++ GameObjects/Upgrades.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject
+{
+    class Upgrades : SpriteGameObject
+    {
+        Boolean activated = false;
+        int health = 100;
+
+        public Upgrades() : base("")
+        {
+            
+        }
+        void HealthMultiplier()
+        {
+            health = 200;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (activated == true)
+            {
+                HealthMultiplier();
+            }
+        }
+
+    }
+}
