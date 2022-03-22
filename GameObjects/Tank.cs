e1d81fa9ab6e2243d9821f0a826a4db992578196
Timo <timo.van.der.thiel@hva.nl>
2022-03-22 15:47:28 +0100

    Second player on map, Movement of second player.

--- GameObjects/Tank.cs
+++ GameObjects/Tank.cs
@@ -6,20 +6,27 @@ using System.Text;
 
 namespace BaseProject
 {
-     class Tank : SpriteGameObject
+     class Tank : GameObjectList
     {
         Vector2 startPosition;
         Vector2 accelerationLeft;
         Vector2 accelerationRight;
         Vector2 accelerationTop;    
         Vector2 accelerationBottom; 
-        public Tank() : base("tank_sprites2")
+        SpriteGameObject player1, player2;
+        public Tank() 
         {
+            player1 = new SpriteGameObject("tankspritesRed");
+            player2 = new SpriteGameObject("tanksprites");
+            Add(player1);
+            Add(player2);
+            player1.Position = new Vector2(50, 100);
+            player2.Position = new Vector2(300, 200);
             startPosition = GameEnvironment.Screen.ToVector2() / 2;
-            accelerationLeft = new Vector2(-20, 0);
-            accelerationRight = new Vector2(20, 0);
-            accelerationTop = new Vector2(0, -20);
-            accelerationBottom = new Vector2(0, 20);
+            accelerationLeft = new Vector2(-10, 0);
+            accelerationRight = new Vector2(10, 0);
+            accelerationTop = new Vector2(0, -10);
+            accelerationBottom = new Vector2(0, 10);
                 
             Reset();
         }
@@ -28,7 +35,7 @@ namespace BaseProject
             base.Reset();
             position = startPosition;
             velocity = Vector2.One;
-            Origin = Vector2.Zero;
+            
         }
 
         public override void Update(GameTime gameTime)
@@ -39,14 +46,23 @@ namespace BaseProject
         public override void HandleInput(InputHelper inputHelper)
         {
             base.HandleInput(inputHelper);
-            if (inputHelper.KeyPressed(Keys.Left))
-                position += accelerationLeft;
-            if (inputHelper.KeyPressed(Keys.Right))
-                position += accelerationRight;
-            if (inputHelper.KeyPressed(Keys.Up))
-                position += accelerationTop;
-            if(inputHelper.KeyPressed(Keys.Down))
-                position += accelerationBottom;
+            if (inputHelper.IsKeyDown(Keys.Left))
+                player1.Position += accelerationLeft;
+            if (inputHelper.IsKeyDown(Keys.Right))
+                player1.Position += accelerationRight;
+            if (inputHelper.IsKeyDown(Keys.Up))
+                player1.Position += accelerationTop;
+            if(inputHelper.IsKeyDown(Keys.Down))
+                player1.Position += accelerationBottom;
+            base.HandleInput(inputHelper);
+            if (inputHelper.IsKeyDown(Keys.A))
+                player2.Position += accelerationLeft;
+            if (inputHelper.IsKeyDown(Keys.D))
+                player2.Position += accelerationRight;
+            if (inputHelper.IsKeyDown(Keys.W))
+                player2.Position += accelerationTop;
+            if (inputHelper.IsKeyDown(Keys.S))
+                player2.Position += accelerationBottom;
         }
     }
 }
