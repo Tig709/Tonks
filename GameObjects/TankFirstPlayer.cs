0cbdfd0b9b33436ff69e0526681909105013791a
Timo <timo.van.der.thiel@hva.nl>
2022-03-26 21:52:21 +0100

    Rotating tanks, extra walls added, collision between tanks created. Tiescreen created, image must been changed!!

--- GameObjects/TankFirstPlayer.cs
+++ GameObjects/TankFirstPlayer.cs
@@ -6,28 +6,23 @@ using System.Text;
 
 namespace BaseProject
 {
-     class TankFirstPlayer : SpriteGameObject
+     class TankFirstPlayer : RotatingSpriteGameObject
     {
         Vector2 startPosition;
         Vector2 accelerationLeft;
         Vector2 accelerationRight;
         Vector2 accelerationTop;    
-        Vector2 accelerationBottom; 
-        /*SpriteGameObject player1, player2;*/
+        Vector2 accelerationBottom;
+        float turningspeed = 1.57f;
+       
         public TankFirstPlayer() : base("tankspritesRed")
         {
-           /* player1 = new SpriteGameObject("tankspritesRed");
-            player2 = new SpriteGameObject("tanksprites");
-            Add(player1);
-            Add(player2);
-            player1.Position = new Vector2(300, 200);
-            player2.Position = new Vector2(50, 100);*/
-            startPosition = GameEnvironment.Screen.ToVector2() / 2;
+            startPosition = new Vector2(0,0);   
             accelerationLeft = new Vector2(-10, 0);
             accelerationRight = new Vector2(10, 0);
             accelerationTop = new Vector2(0, -10);
             accelerationBottom = new Vector2(0, 10);
-                
+
             Reset();
         }
         public override void Reset()
@@ -41,29 +36,38 @@ namespace BaseProject
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            position.X = MathHelper.Clamp(position.X, 0, GameEnvironment.Screen.X - 50);
-            position.Y = MathHelper.Clamp(position.Y, 0, GameEnvironment.Screen.Y - 50);
+            position.X = MathHelper.Clamp(position.X, 55, GameEnvironment.Screen.X - 60);
+            position.Y = MathHelper.Clamp(position.Y, 55, GameEnvironment.Screen.Y - 134);
 
         }
         public override void HandleInput(InputHelper inputHelper)
         {
             base.HandleInput(inputHelper);
+            Origin = Center;
             if (inputHelper.IsKeyDown(Keys.Left))
+            {
                 Position += accelerationLeft;
+            }
+            if (inputHelper.KeyPressed(Keys.M))
+            {
+                Angle += turningspeed;
+            }
+            if (inputHelper.KeyPressed(Keys.N))
+            {
+                Angle -= turningspeed;
+            }
             if (inputHelper.IsKeyDown(Keys.Right))
+            {
                 Position += accelerationRight;
+            }
             if (inputHelper.IsKeyDown(Keys.Up))
+            {
                 Position += accelerationTop;
-            if(inputHelper.IsKeyDown(Keys.Down))
+            }
+            if (inputHelper.IsKeyDown(Keys.Down))
+            {
                 Position += accelerationBottom;
-         /*   if (inputHelper.IsKeyDown(Keys.A))
-                player2.Position += accelerationLeft;
-            if (inputHelper.IsKeyDown(Keys.D))
-                player2.Position += accelerationRight;
-            if (inputHelper.IsKeyDown(Keys.W))
-                player2.Position += accelerationTop;
-            if (inputHelper.IsKeyDown(Keys.S))
-                player2.Position += accelerationBottom;*/
+            }
         }
     }
 }
--- GameStates/PlayingState.cs
+++ GameStates/PlayingState.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,7 @@ namespace BaseProject
     {
         TankFirstPlayer firstPlayerTank;
         TankSecondPlayer secondPlayerTank;
-       GameObjectList unbreakableWall;
+        GameObjectList walls ,walls2, walls3, walls4,walls5;
         public PlayingState()
         {
             
@@ -17,23 +18,51 @@ namespace BaseProject
 
             firstPlayerTank = new TankFirstPlayer();
             this.Add(firstPlayerTank);
+
             secondPlayerTank = new TankSecondPlayer();
-            this.Add(secondPlayerTank); 
-            unbreakableWall = new GameObjectList();
+            this.Add(secondPlayerTank);
+            
+            walls = new GameObjectList();
+            walls2 = new GameObjectList();
+            walls3 = new GameObjectList();  
+            walls4 = new GameObjectList();
+            walls5 = new GameObjectList();
             String[] assetName = {"unbreakable_wall"};
-            int startXPosition = 100,
+            int startXPosition = 200,
                 startYPosition = 0,
-                nWallsPerRow = 3;
+                nWallsPerRow = 6;
             for (int iWallType = 0; iWallType < assetName.Length; iWallType++)
                 for (int iWall = 0; iWall < nWallsPerRow; iWall++)
-                    unbreakableWall.Add(new UnbreakableWall(assetName[iWallType],
-                          new Vector2(startXPosition + iWall, startYPosition + iWallType)));
-            this.Add(unbreakableWall);
+                {
+                    walls2.Add(new UnbreakableWall(assetName[iWallType],
+                         new Vector2(startXPosition + iWall, startYPosition + iWallType)));
+                    walls.Add(new UnbreakableWall(assetName[iWallType],
+                                  new Vector2(startXPosition + iWall+1500, startYPosition + iWallType+800)));
+                    walls3.Add(new UnbreakableWall(assetName[iWallType],
+                                  new Vector2(startXPosition + iWall + 800, startYPosition + iWallType + 400)));
+                    walls4.Add(new UnbreakableWall(assetName[iWallType],
+                                  new Vector2(startXPosition + iWall + 1500, startYPosition + iWallType)));
+                    walls5.Add(new UnbreakableWall(assetName[iWallType],
+                                  new Vector2(startXPosition + iWall, startYPosition + iWallType + 800)));
+                }
+            this.Add(walls);
+            this.Add(walls2);
+            this.Add(walls3);
+            this.Add(walls4);
+            this.Add(walls5);
         }
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
+            if (firstPlayerTank.CollidesWith(secondPlayerTank))
+            {
+                firstPlayerTank.Reset();
+                secondPlayerTank.Reset();
+                GameEnvironment.GameStateManager.SwitchTo("Tie");
+            }
+
 
         }
     }
+    
 }
