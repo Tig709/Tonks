0ac2236151e8f01aa3fc3011b19ff3e17dd92d0f
CvHien <casper.van.hien@hva.nl>
2022-04-26 08:56:38 +0200

    dot

--- GameObjects/Bullet.cs
+++ GameObjects/Bullet.cs
@@ -11,17 +11,19 @@ namespace BaseProject
 
     class Bullet : RotatingSpriteGameObject
     {
+        GameObjectList wall;
         int frameCounter;
         Vector2 startPosition;
         Vector2 startSnelheid;
         float turningspeed = 1.57f;
-        public Bullet(Vector2 startPosition, Vector2 startSnelheid) : base("tank_bullet")
+        public Bullet(Vector2 startPosition, Vector2 startSnelheid, GameObjectList wall) : base("tank_bullet")
         {
 
             origin = Center;
             this.position = startPosition;
             this.startPosition = startPosition;
             this.startSnelheid = startSnelheid;
+            this.wall = wall;
             velocity += startSnelheid;
 
         }
@@ -40,6 +42,36 @@ namespace BaseProject
         }
         public override void Update(GameTime gameTime)
         {
+            foreach (UnbreakableWall wall in wall.Children)
+            {
+                if (this.CollidesWith(wall))
+                {
+                    if (Position.X < wall.Position.X)
+                    {
+                        Console.WriteLine("collision1");
+                        Velocity *= new Vector2(-1, 1);
+                        position.X = wall.Position.X - Width - wall.Width;
+                    }
+                    if (Position.X > wall.Position.X)
+                    {
+                        Console.WriteLine("collision2");
+                        Velocity *= new Vector2(-1, 1);
+                        position.X = wall.Position.X + Width + wall.Width;
+                    }
+                    /* if (Position.Y > wall.Position.Y)
+                     {
+                         Console.WriteLine("collision3");
+                         Velocity *= new Vector2(1, -1);
+                         position.Y = wall.Position.Y - Height - wall.Height;
+                     }
+                     if (Position.Y < wall.Position.Y)
+                     {
+                         Console.WriteLine("collision4");
+                         Velocity *= new Vector2(1, -1);
+                         position.Y = wall.Position.Y + Height + wall.Height;
+                     }*/
+                }
+            }
             base.Update(gameTime);
             WrapScreen();
             frameCounter++; 
