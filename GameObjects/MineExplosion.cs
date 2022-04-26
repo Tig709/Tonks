f1472126bf2367274876ec0717dd76b1aa7dfc85
Tigo <tigo.stam@hva.nl>
2022-04-26 09:38:39 +0200

    start of minexplosion

---
+++ GameObjects/MineExplosion.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject
+{
+    class MineExplosion : SpriteGameObject
+    {
+        Vector2 startPosition;
+        public MineExplosion(String assetNames, Vector2 startPosition) : base(assetNames)
+        {
+            Origin = Center;
+            this.position = startPosition;
+            this.startPosition = startPosition;
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            this.position.X = -1000;
+            this.position.Y = -1000;
+        }
+
+    }
+}
