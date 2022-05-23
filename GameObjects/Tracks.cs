11fc28ac69ae116345405297fe5c4377489ca80f
Timo <timo.van.der.thiel@hva.nl>
2022-05-23 13:20:28 +0200

    Begin for tracks behind tanks

---
+++ GameObjects/Tracks.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject
+{
+  class Tracks : RotatingSpriteGameObject
+    {
+        Vector2 startPosition;
+        public Tracks(Vector2 startPosition) : base("spr_tracks")
+        {
+            this.startPosition = startPosition; 
+        }
+    }
+}
